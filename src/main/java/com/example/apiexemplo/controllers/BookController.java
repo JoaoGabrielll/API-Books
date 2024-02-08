62790798ea5c0539ab3fcd85966978a7fbab5f66
Joao <joaogabrielsb36@gmail.com>
2024-02-07 22:46:33 -0300

    'Subindo'

---
+++ src/main/java/com/example/apiexemplo/controllers/BookController.java
@@ -0,0 +1,16 @@
+package com.example.apiexemplo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @GetMapping("")
+    public String getBooks(){
+        return "oi";
+    }
+}
